9cfc0bef2f545abe8304a22495bcf9a73c861563
andreajakobsen <andreajakobsen@hotmail.dk>
2016-10-02 10:50:41 +0200

    Online database forbindelse
    One.com andreajakobsen.dk database

--- dbconst.php
+++ dbconst.php
@@ -1,11 +1,16 @@
 <?php 
-
+/*
 	const DB_HOST = 'localhost';
 	const DB_USER = 'root';
 	const DB_PASS = '';
 	const DB_NAME = 'phplogin';
 
-
+*/
+	const DB_HOST = 'andreajakobsen.com.mysql';
+	const DB_USER = 'andreajakobsen_';
+	const DB_PASS = 'eELNyYTZ';
+	const DB_NAME = 'andreajakobsen_';
+	
 	$link = new mysqli( DB_HOST, DB_USER, DB_PASS ,DB_NAME );
 
 	if ($link->connect_error) {
